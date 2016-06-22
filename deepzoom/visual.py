6227011055a168bb1754537babd48e4c74c924a4
mananshah99 <manan.shah.777@gmail.com>
2016-06-22 11:07:27 -0400

    Update visual.py

--- deepzoom/visual.py
+++ deepzoom/visual.py
@@ -84,7 +84,7 @@ def get_osr(path):
     hp, hp_name = get_hp()
     print "Loading mask image: ", hp_name
     hp_jet = cv2.applyColorMap(hp, cv2.COLORMAP_JET)
-    alpha = 0.3
+    alpha = 0.5
     img = cv2.addWeighted(img_ori, alpha, hp_jet, 1 - alpha, 1);
     img = img[:, :, (2, 1, 0)] # b,g,r -> r,g,b
     ##
