b6dfe1ea7e871540d4755c8a43efc7071d61cdc3
mananshah99 <manan.shah.777@gmail.com>
2016-07-08 14:17:46 -0400

    ROI facenet core files

---
+++ stage06_fcnSegmentation/roi-facenet/doTrain.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+TOOLS=/home/dywang/dl/caffe/build/tools
+LOG=log
+MODEL=models
+
+mkdir $LOG
+mkdir $MODEL
+
+GLOG_logtostderr=0 \
+GLOG_alsologtostderr=1 \
+GLOG_stderrthreshold=1 \
+GLOG_log_dir=$LOG \
+    $TOOLS/caffe train --solver=./solver.prototxt \
+    --snapshot=models/cnn10_iter_201500.solverstate  \
+    --gpu=2
