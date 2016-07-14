5f76ad7bf824d549d9d35061c107b02e4ad446fc
mananshah99 <manan.shah.777@gmail.com>
2016-07-14 10:25:51 -0400

    Work with fcn generation of pos probability heatmaps

--- stage06_fcnSegmentation/roi-facenet/doTest.sh
+++ stage06_fcnSegmentation/roi-facenet/doTest.sh
@@ -6,9 +6,6 @@ GLOG_alsologtostderr=1 \
 GLOG_stderrthreshold=1 \
 GLOG_log_dir=$LOG \
     $TOOLS/caffe test -model test.prototxt \
-    -weights models.final/round02_cnn10_iter_190000.caffemodel \
+    -weights models/cnn10_iter_217316.caffemodel \
     -iterations 100 \
-    --gpu 0
-
-#    -weights models.final/round02_cnn10_iter_95000.caffemodel \
-#    -weights models.final/round01_face_iter_35000.caffemodel \
+    --gpu 2
--- stage06_fcnSegmentation/roi-facenet/doTrain.sh
+++ stage06_fcnSegmentation/roi-facenet/doTrain.sh
@@ -11,5 +11,5 @@ GLOG_alsologtostderr=1 \
 GLOG_stderrthreshold=1 \
 GLOG_log_dir=$LOG \
     $TOOLS/caffe train --solver=./solver.prototxt \
-    --snapshot=models/cnn10_iter_201500.solverstate  \
+    --snapshot=models/cnn10_iter_202741.solverstate  \
     --gpu=2
