bd2b236b19d93300696b299bfd686769519e2c2f
mananshah99 <manan.shah.777@gmail.com>
2016-07-14 10:29:01 -0400

    Add end to end method 3 implementation; currently training

---
+++ METHOD3/roi-endtoend/doTrain-featurelayer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+TOOLS=/home/dywang/dl/caffe/build/tools
+LOG=log
+MODEL=models
+
+mkdir $LOG
+mkdir $MODEL
+
+GLOG_logtostderr=0 \
+GLOG_alsologtostderr=1 \
+GLOG_stderrthreshold=1 \
+GLOG_log_dir=$LOG \
+    $TOOLS/caffe train --solver=./solver-featurelayer.prototxt \
+    --weights=models/cnn10_iter_217316.caffemodel  \
+    --gpu=2
---
+++ METHOD3/roi-endtoend/generate_result_heatmaps.py
@@ -0,0 +1,58 @@
+import numpy as np
+import sys
+sys.path.insert(0, '../external/caffe/python')
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (10, 10)        # large images
+plt.rcParams['image.interpolation'] = 'nearest'  # don't interpolate: show square pixels
+plt.rcParams['image.cmap'] = 'gray'  # use grayscale output rather than a (potentially misleading) color heatmap
+
+import caffe
+
+caffe.set_device(2)
+caffe.set_mode_gpu()
+
+model_def = 'deploy_fc.prototxt'
+model_weights = 'models/cnn10_iter_217316.caffemodel'
+
+net = caffe.Net(model_def,      # defines the structure of the model
+                model_weights,  # contains the trained weights
+                caffe.TEST)     # use test mode (e.g., don't perform dropout)
+
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+
+transformer.set_transpose('data', (2,0,1))  # move image channels to outermost dimension
+transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
+transformer.set_channel_swap('data', (2,1,0))  # swap channels from RGB to BGR
+transformer.set_mean('data', np.array([104, 117, 123]))
+
+f = open('../lists/mitosis.lst')
+
+i = 0
+# THESE ARE ALL OF THE PATCHES
+for line in f:
+    sample_image_name = line.strip('\n').split(' ')[0]
+    sample_image_output = line.strip('\n').split(' ')[1]
+
+    image = caffe.io.load_image(sample_image_name)
+    image = caffe.io.resize_image(image, (1000, 1000))
+    transformed_image = transformer.preprocess('data', image)
+
+    net.blobs['data'].data[...] = transformed_image
+
+    output = net.forward()
+
+    sample_image_name = sample_image_name[sample_image_name.index('TUPAC-TR-'):-4]
+    a = output['prob']
+
+    print (float(i)/26000, sample_image_name)
+    i += 1
+
+    # (1, 2, 57, 57), the positive one
+    im = a[0][0]
+    
+    plt.imsave('../pos_result_maps/' + sample_image_name + '_class=' + str(sample_image_output) + '.png', im)
