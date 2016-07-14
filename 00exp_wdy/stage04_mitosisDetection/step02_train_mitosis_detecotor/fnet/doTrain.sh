c8366f982c71f633cdb57b2bc16ac286a6ebdb6b
mananshah99 <manan.shah.777@gmail.com>
2016-07-14 15:51:35 -0400

    Fully convolutional facenet for mitosis detection, with improved training data

---
+++ 00exp_wdy/stage04_mitosisDetection/step02_train_mitosis_detecotor/fnet/doTrain.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+TOOLS=/home/dywang/dl/caffe-augmentation/build/tools
+LOG=log
+MODEL=models
+
+mkdir $LOG
+mkdir $MODEL
+
+GLOG_logtostderr=0 \
+GLOG_alsologtostderr=1 \
+GLOG_stderrthreshold=1 \
+GLOG_log_dir=$LOG \
+    $TOOLS/caffe train --solver=./solver.prototxt \
+    --gpu=2
