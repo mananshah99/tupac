caddb1f116881ab8e420c1e48d225d32529fabf9
mananshah99 <manan.shah.777@gmail.com>
2016-07-21 17:12:15 -0400

    Update generation of nuclei masks

--- stage04_mitosisDetection/stage04-1_genNucleiMasks.py
+++ stage04_mitosisDetection/stage04-1_genNucleiMasks.py
@@ -5,6 +5,8 @@ import numpy as np
 from nuclei_detection import nuclei_detection
 from skimage.morphology import disk
 import os.path
+import multiprocessing
+from tqdm import tqdm
 
 # not useful as of yet (Jun 27, 2016)
 def gen_ground_truth_mask(imgfiles):
@@ -27,7 +29,7 @@ def gen_ground_truth_mask(imgfiles):
         skio.imsave(maskfile, mask_img)
         #break
 
-def gen_nuclei_mask(imgfiles):
+def gen_nuclei_mask_old(imgfiles):
     MinPixel = 200
     MaxPixel = 2500
     for imgID, imgfile in enumerate(imgfiles):
@@ -48,10 +50,38 @@ def gen_nuclei_mask(imgfiles):
             print e
             import sys
             sys.exit(0)
-  #          import subprocess
- #           os.remove(imgfile)
-#            print "!!!!! removed " + imgfile
-# imgfiles = [l.strip() for l in os.popen('find /data/dywang/Database/Proliferation/data/mitoses/mitoses_image_data/ -name "*.tif"').readlines()]
-imgfiles = [l.strip() for l in os.popen('find /data/dywang/Database/Proliferation/libs/stage03_deepFeatMaps/results/patches_06-29-16 -name "*.png"').readlines()]
 
-gen_nuclei_mask(imgfiles)
+def gen_nuclei_mask(imgfile):
+    MinPixel = 200
+    MaxPixel = 2500
+    if "mask" in imgfile:
+        return 
+    if os.path.isfile(imgfile[:-4] + "_mask_nuclei.png"):
+        print "Already have " + imgfile
+        return
+    print imgfile
+    try:
+        img = skio.imread(imgfile)
+        bw1 = nuclei_detection(img, MinPixel, MaxPixel)
+        bw1 = bw1 > 0
+        bw1 = ski.morphology.dilation(bw1, disk(5)) > 0
+        maskfile = imgfile[:-4] + "_mask_nuclei.png"
+        skio.imsave(maskfile, bw1*255)
+    except Exception as e:
+        print e
+        import sys
+        sys.exit(0)
+
+# imgfiles = [l.strip() for l in os.popen('find /data/dywang/Database/Proliferation/data/mitoses/mitoses_image_data/ -name "*.tif"').readlines()]
+#imgfiles = [l.strip() for l in os.popen('find /data/dywang/Database/Proliferation/libs/stage03_deepFeatMaps/results/patches_06-29-16 -name "*.png"').readlines()]
+imgfiles = [l.strip() for l in os.popen('find /data/dywang/Database/Proliferation/libs/stage03_deepFeatMaps/results/patches_07-14-16 -name "*).png"').readlines()]
+
+imgfiles.sort(key=lambda x: int(x.split('/')[-1].split('-')[2]))
+
+print len(imgfiles)
+pool = multiprocessing.Pool(20)
+pool.map(gen_nuclei_mask, iter(imgfiles))
+pool.terminate()
+pool.join()
+#pool.imap_unordered(gen_nuclei_mask, iter(imgfiles), chunk_size = )
+#gen_nuclei_mask(imgfiles)
