698bf70e4cbc4f88e1eaee84d6c5d3fd8c9c040d
mananshah99 <manan.shah.777@gmail.com>
2016-07-21 16:50:47 -0400

    Add logs for ROI facenet

--- stage06_fcnSegmentation/roi-facenet/doTrain.sh
+++ stage06_fcnSegmentation/roi-facenet/doTrain.sh
@@ -11,5 +11,5 @@ GLOG_alsologtostderr=1 \
 GLOG_stderrthreshold=1 \
 GLOG_log_dir=$LOG \
     $TOOLS/caffe train --solver=./solver.prototxt \
-    --snapshot=models/cnn10_iter_202741.solverstate  \
-    --gpu=2
+    --snapshot=models/cnn10_iter_217316.solverstate  \
+    --gpu=1
