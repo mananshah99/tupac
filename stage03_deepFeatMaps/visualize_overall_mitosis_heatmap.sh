9175b48342ae52947e02c35b806e8db7463cea49
mananshah99 <manan.shah.777@gmail.com>
2016-07-26 17:47:12 -0400

    Visualizations for mitosis heatmaps on WSI

---
+++ stage03_deepFeatMaps/visualize_overall_mitosis_heatmap.sh
@@ -0,0 +1,2 @@
+python /data/dywang/Database/Proliferation/libs/deepzoom/visual.py /data/dywang/Database/Proliferation/data/TrainingData/small_images-level-2/ --result /data/dywang/Database/Proliferation/libs/stage03_deepFeatMaps/results/mitosis-wsi/ -l 10.35.73.88 -p 5400 --debug
+
