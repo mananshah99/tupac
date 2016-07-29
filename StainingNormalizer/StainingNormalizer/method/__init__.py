20078df659db6aae813ee29cf2a0f638ec3e3e58
mananshah99 <manan.shah.777@gmail.com>
2016-07-29 17:28:50 -0400

    Staining normalizer init modificaiton

--- StainingNormalizer/StainingNormalizer/method/__init__.py
+++ StainingNormalizer/StainingNormalizer/method/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from macenko import macenko, DEFAULT_PARAMETERS as macenko_p
 from vahadane import vahadane, DEFAULT_PARAMETERS as vahadane_p
+from macenko_cut import macenko, DEFAULT_PARAMETERS as macenko_cut
