4cb849d802baec1dd7e8adf30a376eb8878b9f86
mananshah99 <manan.shah.777@gmail.com>
2016-08-01 10:27:12 -0400

    Add script to push all caffemodels to Orchestra

--- do_push_caffemodels.py
+++ do_push_caffemodels.py
@@ -1,10 +1,28 @@
+'''DO THIS:
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_rsa
+'''
+
 import os, sys
 # get all caffemodel files in directory and copy them to orchestra
 from subprocess import call
 
-def do_command(s, q):
-    COMMAND = "sh /home/manan/bin/n rsync -P --rsh=ssh --delete -uar %s ms831@transfer.orchestra.med.harvard.edu:/home/ms831/tupac/%s"%(s, q)
-    print COMMAND
-    #call(COMMAND)
+''' sample 
+sh /home/manan/bin/n rsync -P --rsh=ssh --delete -uar ./stage06_fcnSegmentation/roi-vgg-13/models/vggnet_iter_506.caffemodel ms831@transfer.orchestra.med.harvard.edu:/home/ms831/tupac/./stage06_fcnSegmentation/roi-vgg-13/models/vggnet_iter_506.caffemodel
+'''
 
+def do_command(s):
+    BEFORE = "rsync --recursive emptydir/ ms831@transfer.orchestra.med.harvard.edu:/home/ms831/tupac/%s"%(s[2:s.rfind('/')])
+    COMMAND = "rsync -P --rsh=ssh --delete -uar %s ms831@transfer.orchestra.med.harvard.edu:/home/ms831/tupac/%s "%(s, s[2:])
+    print "\t" + BEFORE
+    call(BEFORE, shell=True)
+    print "\t" + COMMAND
+    call(COMMAND, shell=True)
 
+found = 0
+list_of_files = {}
+for (dirpath, dirnames, filenames) in os.walk('.'):
+    for filename in filenames:
+        if filename.endswith('.caffemodel'): 
+            print "=> Command Running"
+            do_command(os.sep.join([dirpath, filename]))
