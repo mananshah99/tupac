0be9258215d4020956ebef908ed499165ada1fb5
mananshah99 <manan.shah.777@gmail.com>
2016-08-01 22:02:06 -0400

    Remove gnet s2 v4, other mdifications to step02_train_mitosis_detector

--- 00exp_wdy/stage04_mitosisDetection/step02_train_mitosis_detecotor/do_compare.sh
+++ 00exp_wdy/stage04_mitosisDetection/step02_train_mitosis_detecotor/do_compare.sh
@@ -150,4 +150,28 @@ cmd="python ./tools/extract_hp_image.py caffe ${inifile} prob ${inputFolder} ${i
     --gpu"
 
 echo $cmd
+#exec $cmd
+
+###############################
+inifile="$curdir/mnet_s1/conf.ini"
+inputFolder="$curdir/imgs_norm"
+inputList="$curdir/test_nm.lst"
+outputDir="$curdir/result_mnet_s1"
+
+modelLevel=0
+mkdir -p $outputDir
+program_mode="LOCAL"
+cmd="python ./tools/extract_image.py caffe ${inifile} prob ${inputFolder} ${inputList} ${modelLevel} ${outputDir} \
+    --heatmap_level 0 \
+    --mask_image_level 0 \
+    --augmentation 1 \
+    --device_ids 3 \
+    --window_size 100 \
+    --step_size 8 \
+    --batch_size 256 \
+    --group_size 1024 \
+    --program_mode ${program_mode} \
+    --gpu"
+
+echo $cmd
 exec $cmd
--- 00exp_wdy/stage04_mitosisDetection/step02_train_mitosis_detecotor/get_heatmap.sh
+++ 00exp_wdy/stage04_mitosisDetection/step02_train_mitosis_detecotor/get_heatmap.sh
@@ -4,21 +4,21 @@ curdir=`pwd`
 cd /home/dywang/dl/DeepFeat
 root='/home/dywang/Proliferation'
 
-inifile="$curdir/gnet_s2_v3/conf.ini"
+inifile="$curdir/mnet_s1/conf.ini"
 inputFolder="/home/dywang/Proliferation/data/mitoses"
-inputList="/home/dywang/Proliferation/data/mitoses/all_image_withmask_mc13_tr.lst"
+inputList="/home/dywang/Proliferation/data/mitoses/all_image_withmask.lst"
 
-outputDir="$curdir/heatmap_mc13_tr"
+outputDir="$curdir/heatmap_mnet_s1"
 modelLevel=0
 mkdir -p $outputDir
 
 program_mode="LOCAL"
-cmd="python ./tools/extract_hp_image.py caffe ${inifile} prob ${inputFolder} ${inputList} ${modelLevel} ${outputDir} \
+cmd="python ./tools/extract_image.py caffe ${inifile} prob ${inputFolder} ${inputList} ${modelLevel} ${outputDir} \
     --heatmap_level 0 \
     --mask_image_level 0 \
     --augmentation 1 \
     --device_ids 3 \
-    --window_size 100 \
+    --window_size 63 \
     --step_size 4 \
     --batch_size 256 \
     --group_size 1024 \
